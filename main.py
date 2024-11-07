a847bee563f517c40dfcb104a58f796677faeb64
kalleb.abreu <kalleb.abreu@outlook.com>
2024-11-06 23:38:53 -0300

    fix: resolve issues from the previous commit related to git configuration

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-from modules.io import get_filenames, read_tsp_instance
+from modules.io import (
+    get_filenames,
+    read_tsp_instance,
+    get_results_file,
+    save_instance_results
+)
+
 from modules.ctsp import CTSP
+import os
+
+
+
+
 
 ROOT_PATH = "./data/instances"
 filenames = get_filenames(ROOT_PATH)
+results_dir = "./data/results"
+os.makedirs(results_dir, exist_ok=True)
 
-for i in range(len(filenames))[:1]:
-    data = read_tsp_instance(filenames[i][0])
+for i in range(len(filenames))[:18]:
+    data, df_coords = read_tsp_instance(filenames[i][0])
 
     ctsp = CTSP(data, n_clusters=filenames[i][1])
     ctsp.fit()
 
-    # print(data)
+    instance = filenames[i][0].split(ROOT_PATH)[-1][1:]
+    ctsp.print_results(instance)
 
+    results_file = get_results_file(results_dir, i == 0)
+    save_instance_results(results_file, instance, ctsp)
 
-#     instance = filename.split(ROOT_PATH)[-1][1:]
-#     instances.append(instance)
-#     print(f"{instance}")
-
-#     tsp = GeneticAlgorithm(data, population_size=200, mutation_rate=0.02, num_generations=200)
-#     tsp.evolve()
-
-#     cost = tsp.best_cost
-#     costs.append(cost)
-
-#     it = tsp.total_iter
-#     exec_time = tsp.total_time
-#     print(f"   cost: {cost:7.4f}, iter: {it:4}, execution_time: {exec_time:12.2f}s\n")
-
-#     solution = tsp.best_solution
-#     solutions.append(solution)
-
-# df = pd.DataFrame({"instance": instances, "best_OF": costs, "best_SOL": solutions})
-
-# now_str = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-# filename = f"./data/labs/lab7_ga/kalleb_abreu_TSP_ga_{now_str}.csv"
-
-# df.to_csv(filename, index=False)
+    output_path = os.path.join(results_dir, f"{instance}.png")
+    ctsp.plot_solution(df_coords, output_path)
